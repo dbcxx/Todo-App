329db26307b4b9e6365dbc04b2ccd38ba480358a
dolapo <bamideledolapo.s@gmail.com>
2022-06-07 01:07:37 +0100

    used styled component for button UI

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,17 +1,22 @@
-//import React from 'react';
-import styled from 'styled-components'
 
-//import './Button.css';
+import styled from 'styled-components';
+
+
 
 
 const Button = styled.button`
   font: inherit;
+  width: 100%;
   padding: 0.5rem 1.5rem;
   border: 1px solid #042410c2;
   color: white;
   background: #042410c2;
   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
   cursor: pointer;
+
+  @media (min-width: 768px) {
+    width: auto;
+  }
 
   &:focus {
     outline: none;
